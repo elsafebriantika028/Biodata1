ec7389ce8367d53694fe69dc4dd91a8830232b6b
elsafebriantika028 <elsafebriantikaa@gmail.com>
2020-09-17 14:24:40 +0700

    elsa

---
+++ biodataelsa.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf ("\t===BIODATA KU====\n");
+	printf ("Nama : Elsa Febriantika\n");
+	printf ("NPM : 19081010028\n");
+	printf ("Alamat : Kedungrejo, megaluh, Jombang\n");
+	printf ("TTL: Jombang, 23 Februari 2001\n");
+	printf ("Jurusan : Informatika - UPN VETERAN JAWA TIMUR");
+	
+	return 0;
+}
